41943684ffea083746b76f543d08656f00a0f23c
SkyRavenS1lver <revandra.aryo.dwi.krisnandaru@mail.ugm.ac.id>
2022-10-20 14:58:20 +0700

    Lanjutan

--- app/src/main/java/com/example/myapplication/Api.java
+++ app/src/main/java/com/example/myapplication/Api.java
@@ -1,9 +1,15 @@
 package com.example.myapplication;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface Api {
     @GET("Beer")
     Call<Beer> getRandomBeer();
+
+    @GET("Beer")
+    Call<List<Beer>> getBeers(@Query("size") int number);
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -16,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv1 = findViewById(R.id.TV1);
         getRandomData();
+        getBeers();
     }
     public void getRandomData(){
         Call<Beer> caller = RetrofitClient.getInstance().getMyApi().getRandomBeer();
@@ -33,5 +37,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+    }
+    public void getBeers(){
+        Call<List<Beer>> caller = RetrofitClient.getInstance().getMyApi().getBeers( 2);
+        caller.enqueue(new Callback<List<Beer>>() {
+            @Override
+            public void onResponse(Call<List<Beer>> call, Response<List<Beer>> response) {
+                List<Beer> beerList = response.body();
+                String beer = " ";
+                        for (int i = 0 ; i<beerList.size();i++){
+                            Beer currentBeer = beerList.get(i);
+                            beer = beer+","+currentBeer.getBrand();
+                        }
+                tv1.setText(beer);
+            }
+
+            @Override
+            public void onFailure(Call<List<Beer>> call, Throwable t) {
+
+            }
+        });
     }
 }
