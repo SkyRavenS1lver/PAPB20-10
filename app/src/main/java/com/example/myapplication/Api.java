adced831fcd85e3e4ceaeab30b4fbd28ac4bf932
SkyRavenS1lver <revandra.aryo.dwi.krisnandaru@mail.ugm.ac.id>
2022-10-20 15:08:14 +0700

    Lanjutan

--- app/src/main/java/com/example/myapplication/Api.java
+++ app/src/main/java/com/example/myapplication/Api.java
@@ -7,9 +7,9 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface Api {
-    @GET("Beer")
+    @GET("beers")
     Call<Beer> getRandomBeer();
 
-    @GET("Beer")
+    @GET("beers")
     Call<List<Beer>> getBeers(@Query("size") int number);
 }
